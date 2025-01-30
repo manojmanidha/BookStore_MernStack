6d202c76d390d4a5635e1e40b43611048635b9e9
Manoj K S <manojks.062001@gmail.com>
2025-01-30 12:39:14 +0530

    fromtend page creation home and show details paes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Routes , Route} from 'react-router-dom';
+import CreateBooks from './pages/CreateBooks';
+import DeleteBook from './pages/DeleteBook';
+import EditBook from './pages/EditBook';
+import Home from './pages/Home';
+import ShowBook from './pages/ShowBook';
 
 const App = () => {
   return (
-    <div className='bg-red-200 flex item-center'>App</div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/books/create' element={<CreateBooks/>}/>
+      <Route path='/books/delete/:id' element={<DeleteBook/>}/>
+      <Route path='/books/edit/:id' element={<EditBook/>}/>
+      <Route path='/books/details/:id' element={<ShowBook/>}/>
+    </Routes>
   )
 }
 
